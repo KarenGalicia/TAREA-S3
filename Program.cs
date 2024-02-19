12c3b0225bb630e0d251bb986ec2c433206593b6
Karen Galicia <159975831+KarenGalicia@users.noreply.github.com>
2024-02-18 21:14:05 -0600

    Ahora si

--- Program.cs
+++ Program.cs
@@ -2,42 +2,40 @@
 
 class Program
 {
-	static void Main(string[] arg)
-	{
-		Console.WriteLine("**1.CALCULA EL MAYOR DE TRES NÚMEROS**");
-		Console.WriteLine("--------------------------");
+    static void Main(string[] arg)
+    {
+        Console.WriteLine("**1.CALCULA EL MAYOR DE TRES NÚMEROS**");
+        Console.WriteLine("--------------------------");
 
-		int num1, num2, num3;
-		int mayor;
+        int num1, num2, num3;
+        int mayor;
 
-		Console.WriteLine("Introduzca el primer número:");
+        Console.WriteLine("Introduzca el primer número:");
 
-#pragma warning disable CS8604
-		num1 = int.Parse(s:Console.ReadLine());
+        num1 = Convert.ToInt32(Console.ReadLine());
 
-		Console.WriteLine("Introduzca el segundo número:");
+        Console.WriteLine("Introduzca el segundo número:");
 
-		num2 = int.Parse(s:Console.ReadLine()) ;
+        num2 = Convert.ToInt32(Console.ReadLine());
 
-		Console.WriteLine("Introduzca el tercer número:");
+        Console.WriteLine("Introduzca el tercer número:");
 
-#pragma warning disable CA1305 
-		num3 = int.Parse(s: Console.ReadLine());
+        num3 = Convert.ToInt32(Console.ReadLine());
 
 
-		mayor = num1;
-		if (num2 > mayor)
-		{
-			mayor = num2;
-		}
-		if (num3 > mayor)
-		{
-			mayor = num3;
-		}
+        mayor = num1;
+        if (num2 > mayor)
+        {
+            mayor = num2;
+        }
+        if (num3 > mayor)
+        {
+            mayor = num3;
+        }
 
-		// Mostrar el mayor
-		Console.ForegroundColor = ConsoleColor.DarkBlue;
-		Console.WriteLine("El mayor de los tres números es: " + mayor);
-		Console.ResetColor();
-	}
+        // Mostrar el mayor
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("El mayor de los tres números es: " + mayor);
+        Console.ResetColor();
+    }
 }
